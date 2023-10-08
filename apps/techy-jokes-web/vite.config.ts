298274b03a936e2654c61ea5f40b47610647079d
Emily Xiong <xiongemi@gmail.com>
2023-10-08 16:59:53 -0400

    make web vite work with prod

--- apps/techy-jokes-web/vite.config.ts
+++ apps/techy-jokes-web/vite.config.ts
@@ -1,14 +1,49 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import * as esbuild from 'esbuild';
+import { readFileSync } from 'fs';
+
+const extensions = [
+  '.web.tsx',
+  '.tsx',
+  '.web.ts',
+  '.ts',
+  '.web.jsx',
+  '.jsx',
+  '.web.js',
+  '.js',
+  '.css',
+  '.json',
+];
+
+const rollupPlugin = (matchers: RegExp[]) => ({
+  name: 'js-in-jsx',
+  load(id: string) {
+    if (
+      matchers.some((matcher) => matcher.test(id)) &&
+      id.endsWith('.js')
+    ) {
+      const file = readFileSync(id, { encoding: 'utf-8' });
+      return esbuild.transformSync(file, { loader: 'jsx', jsx: 'automatic' });
+    }
+  },
+});
 
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/techy-jokes-web',
 
   resolve: {
+    extensions: extensions,
     alias: {
       'react-native': 'react-native-web',
       'react-native-svg': 'react-native-svg-web',
+    },
+  },
+
+  build: {
+    rollupOptions: {
+      plugins: [rollupPlugin([/react-native-vector-icons/])],
     },
   },
 
@@ -22,7 +57,21 @@ export default defineConfig({
     host: 'localhost',
   },
 
-  plugins: [react(), nxViteTsPaths()],
+  optimizeDeps: {
+    exclude: ['react-native-vector-icons', 'react-native-safe-area-context'],
+  },
+
+  plugins: [
+    react({
+      babel: {
+        plugins: [
+          '@babel/plugin-proposal-export-namespace-from',
+          'react-native-reanimated/plugin',
+        ],
+      },
+    }),
+    nxViteTsPaths(),
+  ],
 
   // Uncomment this if you are using workers.
   // worker: {
