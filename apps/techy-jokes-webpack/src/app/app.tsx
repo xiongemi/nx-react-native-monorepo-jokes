e104e38a6dbcad195e9f61fc46f4364973b82ab7
Emily Xiong <xiongemi@gmail.com>
2023-10-30 11:56:03 -0400

    add techy-jokes-webpack

---
+++ apps/techy-jokes-webpack/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="techy-jokes-webpack" />
+    </div>
+  );
+}
+
+export default App;
---
+++ apps/techy-jokes-webpack/webpack.config.js
@@ -0,0 +1,16 @@
+const { composePlugins, withNx } = require('@nx/webpack');
+const { withReact } = require('@nx/react');
+
+// Nx plugins for webpack.
+module.exports = composePlugins(withNx(), withReact(), (config) => {
+  // Update the webpack config as needed here.
+  // e.g. `config.plugins.push(new MyPlugin())`
+  config.externals = [
+    'react-native-vector-icons',
+    'react-native-safe-area-context',
+  ];
+  config.resolve.alias = {
+    'react-native': 'react-native-web',
+  };
+  return config;
+});
