f2ee5a2acd39acbd9a7928579fae4069575073be
Emily Xiong <xiongemi@gmail.com>
2025-08-19 21:07:18 -0400

    upgrade @react-native-community/cli to 20.0.0

--- apps/techy-jokes-webpack/.storybook/main.ts
+++ apps/techy-jokes-webpack/.storybook/main.ts
@@ -2,11 +2,7 @@ import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
   stories: ['../src/app/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@nx/react/plugins/storybook',
-  ],
+  addons: ['@nx/react/plugins/storybook'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {},
